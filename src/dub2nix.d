a3911a2537a8935effb8c56498abfd918dc34ec7
Lionello Lunesu <lio+git@lunesu.com>
2019-04-21 15:08:43 +0800

    Using getopt and libcurl

--- src/dub2nix.d
+++ src/dub2nix.d
@@ -17,19 +17,36 @@ struct DubRepo {
     string project;
 }
 
-/// Query Dub registry for the repository information of a package
-auto findRepo(string pname) @trusted {
-    const url = "http://code.dlang.org/packages/" ~ pname ~ ".json";
+private string packageRegistry = "http://code.dlang.org/packages/";
+
+private auto download(string url) @trusted {
 version(none) {
     // This works, but causes "leaking eventcore driver" warnings at shutdown
     import vibe.http.client : requestHTTP;
     scope res = requestHTTP();
-    const json = res.readJson();
+    return res.readJson();
 } else {
-    import std.net.curl: get;
-    // parseJsonString takes immutable string, so need the .idup here
-    const json = parseJsonString(get(url).idup);
+    import std.net.curl: get, HTTP;
+    auto http = HTTP();
+    // Using gzip saves A LOT of traffic, ~40x
+    http.addRequestHeader("accept", "application/json");
+    http.addRequestHeader("accept-encoding", "deflate");
+    const data = get(url, http);
+    // Only accepting application/json, so anything else must be compressed
+    if (data[0] != '{') {
+        import std.zlib : uncompress;
+        return parseJsonString(cast(string)uncompress(data));
+    } else {
+        // parseJsonString takes immutable string, so need the .idup here
+        return parseJsonString(data.idup);
+    }
 }
+}
+
+/// Query Dub registry for the repository information of a package
+auto findRepo(string pname) @safe {
+    const url = packageRegistry ~ pname ~ ".json";
+    const json = download(url);
     return deserializeJson!DubRepo(json["repository"]);
 }
 
@@ -46,7 +63,7 @@ struct NixPrefetchGit {
 auto nixPrefetchGit(string url, string rev) @safe {
     import std.process : executeShell;
     return deserializeJson!NixPrefetchGit(
-        executeShell("nix-prefetch-git --fetch-submodules --quiet " ~ url ~ " " ~ rev).output
+        executeShell("nix-prefetch-git --quiet " ~ url ~ " " ~ rev).output
     );
 }
 
@@ -54,12 +71,12 @@ struct DubDep {
     NixPrefetchGit fetch;           /// like Go deps.nix
 }
 
-/// Fetch the repo information for package `key` and version `value`
-auto prefetch(Tuple)(in Tuple pair) @safe {
-    const repo = findRepo(pair.key);
+/// Fetch the repo information for package `pname` and version `ver`
+auto prefetch(string pname, string ver) @safe {
+    const repo = findRepo(pname);
     assert(repo.kind == "github");
     const url = "https://" ~ repo.kind ~ ".com/" ~ repo.owner ~ '/' ~ repo.project ~ ".git";
-    const tag = "v" ~ pair.value;
+    const tag = "v" ~ ver;
     auto set = nixPrefetchGit(url, tag);
     // Overwrite the sha1 ref with the tag instead, so we have the version info as well
     set.rev = tag;
@@ -68,7 +85,7 @@ auto prefetch(Tuple)(in Tuple pair) @safe {
 }
 
 /// Convert D string to Nix string literal
-auto toNixString(in string s) pure @safe {
+auto toNixString(in string s, int indent = 0) pure @safe {
     if (s is null) {
         return "null";
     } else if (s.indexOfAny("\"\n") >= 0)
@@ -85,7 +102,7 @@ unittest {
 }
 
 /// Convert D bool to Nix boolean literal
-auto toNixString(bool b) pure @safe {
+auto toNixString(bool b, int indent = 0) pure @safe {
     return b ? "true" : "false";
 }
 
@@ -94,14 +111,17 @@ unittest {
     static assert(toNixString(false) == "false");
 }
 
+private enum INDENT = "                                                               ";
+
 /// Convert D struct to Nix set
-auto toNixString(T)(in T pod) pure @safe if (is(T == struct)) {
+auto toNixString(T)(in T pod, int indent = 0) pure @safe if (is(T == struct)) {
+    string prefix = INDENT[0..indent * 2 + 2];
     string set = "{\n";
     foreach(i, ref key; pod.tupleof) {
         const id = __traits(identifier, pod.tupleof[i]);
-        set ~= id ~ " = " ~ toNixString(key) ~ ";\n";
+        set ~= prefix ~ id ~ " = " ~ toNixString(key, indent + 1) ~ ";\n";
     }
-    return set ~ "}";
+    return set ~ INDENT[0..indent * 2] ~ "}";
 }
 
 unittest {
@@ -110,12 +130,13 @@ unittest {
 }
 
 /// Convert D AArray to Nix set
-auto toNixString(T)(in T[string] aa) pure @safe {
+auto toNixString(T)(in T[string] aa, int indent = 0) pure @safe {
+    string prefix = INDENT[0..indent * 2 + 2];
     string set = "{\n";
     foreach(id, ref key; aa) {
-        set ~= id ~ " = " ~ toNixString(key) ~ ";\n";
+        set ~= prefix ~ id ~ " = " ~ toNixString(key, indent + 1) ~ ";\n";
     }
-    return set ~ "}";
+    return set ~ INDENT[0..indent * 2] ~ "}";
 }
 
 unittest {
@@ -124,10 +145,10 @@ unittest {
 
 /// Convert D array to Nix list
 import std.range : isForwardRange;
-auto toNixString(R)(in R range) pure @safe if (isForwardRange!R && !is(R : string)) {
+auto toNixString(R)(in R range, int indent = 0) pure @safe if (isForwardRange!R && !is(R : string)) {
     string list = "[ ";
     foreach(const ref item; range) {
-        list ~= toNixString(item) ~ " ";
+        list ~= toNixString(item, indent) ~ " ";
     }
     return list ~ "]";
 }
@@ -139,22 +160,58 @@ unittest {
 // enum Selections = deserializeJson!DubSelections(import("./dub.selections.json"));
 // static assert(Selections.fileVersion == 1);
 
-void main(string[] args) {
+int main(string[] args) {
     import std.stdio : writeln;
     import std.parallelism : taskPool;
     import std.array : byPair, array;
-    import std.file : readText, write;
+    import std.file : readText, write, FileException;
+    import std.getopt: getopt, defaultGetoptPrinter;
 
-    string filename = args.length == 2 ? args[1] : "./dub.selections.json";
-    const selections = deserializeJson!DubSelections(readText(filename));
-    assert(selections.fileVersion == 1);
+    string input = "./dub.selections.json", deps = "./dub.selections.nix";
+    auto result = getopt(args,
+        "input|i|in", "Path of selections JSON; defaults to " ~ input, &input,
+        "registry|r", "URL to Dub package registry; default " ~ packageRegistry, &packageRegistry,
+        "deps|d", "Output Nix file with dependencies; defaults to " ~ deps, &deps);
 
-    const nix = "# This file was generated by dub2nix v0.1.0\n"
-        ~ toNixString(taskPool.amap!prefetch(selections.versions.byPair.array));
+    if (result.helpWanted || args.length != 2) {
+        defaultGetoptPrinter(`Usage: dub2nix [OPTIONS] COMMAND
 
-    if (args.length == 3) {
-        write(args[2], nix.representation);
-    } else {
-        writeln(nix);
+Create Nix derivations for Dub packages.
+
+Commands:
+  save      Write Nix files for Dub project
+
+Options:`, result.options);
+        return 0;
+    }
+
+    try {
+        const stdout = deps == "-";
+        const selections = deserializeJson!DubSelections(readText(input));
+        assert(selections.fileVersion == 1);
+
+        static auto progress(T)(in T pair) {
+            debug writeln("# Fetching ", pair.key);
+            return prefetch(pair.key, pair.value);
+        }
+
+        const nix = "# This file was generated by https://github.com/lionello/dub2nix v0.1.0\n"
+            ~ toNixString(taskPool.amap!progress(selections.versions.byPair.array));
+
+        if (stdout) {
+            writeln(nix);
+        } else {
+            write(deps, nix.representation);
+        }
+        debug writeln("# Done.");
+        return 0;
+    } catch (Exception e) {
+        debug {
+            // Only dump callstack in debug builds
+            writeln(e.toString());
+        } else {
+            writeln(e.message);
+        }
+        return 1;
     }
 }
