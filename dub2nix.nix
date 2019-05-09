aa398a3f1ceb08d9359813d8b00348fb6441e92c
Lionello Lunesu <lio+git@lunesu.com>
2019-05-09 15:28:24 +0100

    Add option -o --output

--- dub2nix.nix
+++ dub2nix.nix
@@ -6,7 +6,7 @@ mkDubDerivation {
     src = ./.;
     # dubJSON = ./dub.json;
     # selections = ./dub.selections.nix;
-    version = "0.1.0";
+    version = "0.2.0";
     meta = with pkgs; {
         maintainers = [ lib.maintainers.lionello ];
     };
