7f99aac68343930402e3d3739d4bf45f96a5f993
Lionello Lunesu <lio+git@lunesu.com>
2020-08-13 21:08:16 +0800

    Add license to dub2nix

--- dub2nix.nix
+++ dub2nix.nix
@@ -6,9 +6,11 @@ mkDubDerivation {
     src = ./.;
     # dubJSON = ./dub.json;
     # selections = ./dub.selections.nix;
-    version = "0.2.1";
+    version = "0.2.2";
     propagatedBuildInputs = [ pkgs.nix-prefetch-git ];
-    meta = with pkgs; {
-        maintainers = [ lib.maintainers.lionello ];
+    meta = with pkgs.stdenv.lib; {
+       homepage = "https://github.com/lionello/dub2nix";
+       maintainers = [ lib.maintainers.lionello ];
+       license = licenses.mit;
     };
 }
